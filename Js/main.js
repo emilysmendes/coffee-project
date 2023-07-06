a86d47698e36b8d3e3ac68231e074f39c9c391da
Emily Svetcos <emilysvetcos@gmail.com>
2023-07-06 18:21:56 -0500

    changed names

--- Js/main.js
+++ Js/main.js
@@ -61,20 +61,20 @@ function newCoffee (e) {
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+    {id: 1, name: 'Felix Felicis', roast: 'light'},
+    {id: 2, name: 'Love Potion', roast: 'light'},
+    {id: 3, name: 'Revive Elixir', roast: 'light'},
+    {id: 4, name: 'Mandrake Restorative Potion', roast: 'medium'},
+    {id: 5, name: 'Truth Serum', roast: 'medium'},
+    {id: 6, name: 'Wolfsbane', roast: 'medium'},
+    {id: 7, name: 'Skele-Gro', roast: 'dark'},
+    {id: 8, name: 'Draught of Living Death', roast: 'dark'},
+    {id: 9, name: 'Murtlap Essence', roast: 'dark'},
+    {id: 10, name: 'Polyjuice Potion', roast: 'dark'},
+    {id: 11, name: 'Rat Tonic', roast: 'dark'},
+    {id: 12, name: 'Venomous Tentacula Juice', roast: 'dark'},
+    {id: 13, name: 'Veritaserum', roast: 'dark'},
+    {id: 14, name: 'Wiggenweld Potion', roast: 'dark'},
 ];
 
 
